a27ab82f79c146ae4ea6818f9abea62ce175863a
fatema <fatema@gmail.com>
2023-07-14 09:54:07 +0530

    this is the first commit from this project

---
+++ demodocker/src/main/java/springdocker/demodocker/DemodockerApplication.java
@@ -0,0 +1,13 @@
+package springdocker.demodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodockerApplication.class, args);
+	}
+
+}
---
+++ demodocker/src/main/java/springdocker/demodocker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package springdocker.demodocker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemodockerApplication.class);
+	}
+
+}
